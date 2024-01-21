dc71f8383982c5d3cf33d720c68934744a82054d
k-f-m <55965735+k-f-m@users.noreply.github.com>
2024-01-22 00:11:09 +0100

    Create new NUnit test project using .NET CLI

---
+++ playwright-nunit-examples/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace playwright_nunit_examples;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
